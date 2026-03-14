91012aca11bc0f5e8612ab3e97eff2530ad8c07c
Logesh-bruce <logeshmg432006@gmail.com>
2026-03-14 21:56:00 +0530

    fix: add CORS headers for Vercel frontend

--- server.ts
+++ server.ts
@@ -19,6 +19,14 @@ async function startServer() {
 
   app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
   // Initialize DB
   const db = await getDb();
 
